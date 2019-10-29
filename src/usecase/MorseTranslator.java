2d24633474cd3dcf87622cf3c9d7921b9959ecaa
misial97 <35803878+misial97@users.noreply.github.com>
2019-10-29 22:16:54 +0100

    Inicializacion del arbol correcta.

--- src/usecase/MorseTranslator.java
+++ src/usecase/MorseTranslator.java
@@ -1,9 +1,24 @@
 package usecase;
 
 
+import material.Position;
+import material.tree.binarytree.LinkedBinaryTree;
+import material.tree.iterators.InorderBinaryTreeIterator;
+
 public class MorseTranslator {
     //TODO: Practica 3 Ejercicio 3
+    //derecha -
+    //izquierda .
+    private final char LEFT_DOT = '.';
+    private final char RIGHT_DASH = '-';
+    private LinkedBinaryTree<String> tree;
 
+    public MorseTranslator(){
+
+        char[] standardCharset = {};
+        String[] standardCodes = {};
+        treeInitializer(standardCharset, standardCodes);
+    }
     /**
      * Generates a new MorseTranslator instance given two arrays:
      * one with the character set and another with their respective
@@ -13,9 +28,53 @@ public class MorseTranslator {
      * @param codes
      */
     public MorseTranslator(char[] charset, String[] codes) {
-        throw new RuntimeException("Not yet implemented");
+        this.tree = new LinkedBinaryTree<>();
+        this.tree.addRoot("start");
+        treeInitializer(charset, codes);
     }
 
+    private void treeInitializer(char[] charset, String[] codes){
+        Position<String> position;
+        int i = 0;
+        for(String code: codes){
+            position = this.tree.root();
+            while(!code.equals("")){
+                if (code.charAt(0) == this.LEFT_DOT) {
+                    if(code.length()==1){
+                        String toInsert = "" + charset[i];
+                        if(!this.tree.hasLeft(position))
+                            position = this.tree.insertLeft(position, toInsert);
+                        else {
+                            Position<String> leftChild = this.tree.left(position);
+                            this.tree.replace(leftChild, toInsert);
+                        }
+                    }else {
+                        if(!this.tree.hasLeft(position))
+                            position = this.tree.insertLeft(position, "");
+                        else
+                            position = this.tree.left(position);
+                    }
+                }else if (code.charAt(0) == this.RIGHT_DASH) {
+                    if(code.length()==1){
+                        String toInsert = "" + charset[i];
+                        if(!this.tree.hasRight(position))
+                            position = this.tree.insertRight(position, toInsert);
+                        else {
+                            Position<String> rightChild = this.tree.right(position);
+                            this.tree.replace(rightChild, toInsert);
+                        }
+                    }else {
+                        if(!this.tree.hasRight(position))
+                            position = this.tree.insertRight(position, "");
+                        else
+                            position = this.tree.right(position);
+                    }
+                }
+                code = code.substring(1);
+            }
+            i++;
+        }
+    }
     /**
      * Decodes a String with a message in morse code and returns
      * another String in plaintext. The input String may contain
@@ -40,5 +99,11 @@ public class MorseTranslator {
         throw new RuntimeException("Not yet implemented");
     }
 
+    public void imprime(){
+        InorderBinaryTreeIterator<String> it = new InorderBinaryTreeIterator<>(this.tree);
+        while(it.hasNext()){
+            System.out.println(it.next().getElement());
+        }
 
+    }
 }
